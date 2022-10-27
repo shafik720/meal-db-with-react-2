b833017f93810fafe8c157f3bb420eec2d614fa6
shafik720 <shafikrasel5@gmail.com>
2022-10-27 14:00:39 +0600

    fdgsfgs

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Meal from '../Meal/Meal';
 import './Body.css'
 
 const Body = () => {
+
+    const[meals, setMeals] = useState([]);
+    useEffect(()=>{
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=a')
+        .then(res=>res.json())
+        .then(data=>setMeals(data.meals))
+    },[])
     return (
         <div className="body-div">
             <div className="left-side">
-                <Meal></Meal>
+                {
+                    meals.map(index=><Meal></Meal>)
+                }
             </div>
             <div className="right-side">
                 <Cart></Cart>
