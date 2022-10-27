06822b3db8f9e6508b86efd9d3a8de8895ad5d55
shafik720 <shafikrasel5@gmail.com>
2022-10-27 14:12:37 +0600

    fdgsfgs

--- src/Components/Meal/Meal.js
+++ src/Components/Meal/Meal.js
@@ -5,9 +5,19 @@ const Meal = (props) => {
     console.log(props.index);
     const {idMeal, strArea, strCategory, strMeal, strMealThumb} = props.index;
     return (
-        <div>
+        <div className="food-card">
             <div className="card-img">
                 <img src={strMealThumb} alt=""/>
+            </div>
+            <div className="card-body">
+                <div className="body-left">
+                    <h3>{strMeal}</h3>
+                    <button>Order</button>
+                </div>
+                <div className="body-right">
+                    <p>{strCategory}</p>
+                    <p>{strArea}</p>
+                </div>
             </div>
         </div>
     );
