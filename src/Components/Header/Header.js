48c1caa57b626bfffe406a20afeba7a65ae09b66
shafik720 <shafikrasel5@gmail.com>
2022-10-27 13:00:13 +0600

    dfgfdsg

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';   
+import { faUtensilSpoon } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
         <div className="header-div">
-            <h2>Hello From Header</h2>
+            <div className="left-side">
+            <FontAwesomeIcon icon={faUtensilSpoon} />
+            <h2>Food Era</h2>
+            </div>
+            <div className="right-side">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Menu</a>
+                <a href="">Contact</a>
+                <a href="">Shop</a>
+            </div>
         </div>
     );
 };
